5c6f19ee2eabf307a342b360ce072d11f7a31f10
navjotmaan <navjotmaan137@gmail.com>
2025-11-12 09:56:47 +0000

    add condition to check invalid or empty value

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function capitalize(string) {
+    if (typeof string !== 'string' || string.length === 0) return '';
     return string[0].toUpperCase() + string.slice(1);
 }
+
+module.exports = {
+    capitalize
+};
